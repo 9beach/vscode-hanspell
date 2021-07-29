71c6f3832e8de3fc812d739ce1c25547bdc1862f
9beach <9beach@gmail.com>
2021-07-29 13:50:05 +0900

    Change DocumentSelector from  ['markdown', 'plaintext'] to '*'

--- src/extension.ts
+++ src/extension.ts
@@ -21,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
   // `vscode-hanspell.fixAllTypos`.
   context.subscriptions.push(
     vscode.languages.registerCodeActionsProvider(
-      ['markdown', 'plaintext'],
+      '*',
       new HanspellCodeAction(),
       { providedCodeActionKinds: HanspellCodeAction.providedCodeActionKinds },
     ),
