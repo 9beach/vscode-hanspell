a1a6dbad3bd03b796ba6dc1734e1b8dfe7e31cce
9beach <9beach@gmail.com>
2021-08-14 17:51:11 +0900

    Change `HanspellTypo.info` of `.hanspell-typos`

--- src/typo-db.ts
+++ src/typo-db.ts
@@ -30,7 +30,7 @@ export class HanspellTypoDB {
           return {
             token: lr[0],
             suggestions: [lr[1]],
-            info: '사용자 맞춤법 데이터베이스',
+            info: '사용자 맞춤법 정의',
             local: true,
           };
         });
